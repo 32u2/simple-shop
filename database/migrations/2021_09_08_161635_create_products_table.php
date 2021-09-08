c659d46dcb263bd729d6fe0a99ba5eae1c522f77
32u2 <admin@instarange.com>
2021-09-08 18:29:24 +0200

    products migration + model fillables

--- database/migrations/2021_09_08_161635_create_products_table.php
+++ database/migrations/2021_09_08_161635_create_products_table.php
@@ -16,6 +16,11 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            // $table->user_id(); // not required, we are assuming single admin user..
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->text('description')->nullable(); // not required for now
+            $table->decimal('price');
         });
     }
 
