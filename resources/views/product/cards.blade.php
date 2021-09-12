c2cb3a53de03bb16b5d23599802af596465d5ece
32u2 <admin@instarange.com>
2021-09-12 11:22:28 +0200

    equalize card height

--- resources/views/product/cards.blade.php
+++ resources/views/product/cards.blade.php
@@ -10,7 +10,7 @@
                         <div class="p-4 w-80">
                             <div class="max-w-sm bg-white rounded-lg overflow-hidden shadow-lg">
                                 <img class="w-full" src="{{ $p->image_path }}" alt="{{ $p->name }} image">
-                                <div class="px-6 py-4">
+                                <div class="px-6 py-4 h-28">
                                     <div class="font-bold text-lg mb-2">{{$p->name}}</div>
                                     <p class="text-gray-700 text-sm">{{$p->description}}</p>
                                 </div>
@@ -19,9 +19,11 @@
                                         &#163;{{ number_format($p->price, 2, '.', ',') }}
                                     </span>
                                     @auth
-                                    <span class="inline-block bg-gray-800 rounded-full w-16 px-3 py-1 text-sm font-semibold text-gray-100 mr-2 float-right text-center">
-                                        Edit
-                                    </span>
+                                    <a href="{{ route('update-product', $p->id) }}">
+                                        <span class="inline-block bg-gray-800 hover:bg-purple-800 rounded-full w-16 px-3 py-1 text-sm font-semibold text-gray-100 mr-2 float-right text-center">
+                                            Edit
+                                        </span>
+                                    </a>
                                     @endauth
                                 </div>
                             </div>
